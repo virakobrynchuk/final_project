0ffc71944a8d92d5c73b9d6c53e57534184ccc6a
vira <virakobrynchuk@gmail.com>
2020-10-23 20:31:49 +0300

    create login_page and locator

---
+++ pages/locators.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    BASKET_BUTTON = (By.CSS_SELECTOR, ".basket-mini a")
+
+
+class LoginPageLocators():
+    LOGIN_USERNAME_FORM = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASS_FORM = (By.CSS_SELECTOR, "#id_login-password")
+    REG_EMAIL_FORM = (By.CSS_SELECTOR, "#id_registration-email")
+    REG_PASS1_FORM = (By.CSS_SELECTOR, "#id_registration-password1")
+    REG_PASS2_FORM = (By.CSS_SELECTOR, "#id_registration-password2")
+    REG_BUTTON = (By.CSS_SELECTOR, "#register_form .btn")
---
+++ pages/login_page.py
@@ -0,0 +1,26 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert "login" in self.browser.current_url, "Login page not open"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_USERNAME_FORM) \
+               and self.is_element_present(*LoginPageLocators.LOGIN_PASS_FORM), \
+            "Login or pass form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REG_EMAIL_FORM) \
+               and self.is_element_present(*LoginPageLocators.REG_PASS1_FORM) \
+               and self.is_element_present(*LoginPageLocators.REG_PASS2_FORM), \
+            "Registration form is not presented"
